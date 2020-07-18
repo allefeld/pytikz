f14707adb1f002634f986242733b1492260eaf2d
Carsten Allefeld <carsten.allefeld@city.ac.uk>
2020-07-18 11:44:57 +0100

    exact tick values, "Decimal" format labels

--- tikz/labeler.py
+++ tikz/labeler.py
@@ -1,6 +1,7 @@
-from math import inf, log10, ceil, floor
+from math import log10, ceil, floor
 from itertools import count
 from sys import float_info
+from decimal import Decimal as D
 
 
 class ExtendedWilkinson:
@@ -17,8 +18,13 @@ class ExtendedWilkinson:
     https://rdrr.io/rforge/labeling/src/R/labeling.R
     """
 
-    Q = [1, 5, 2, 2.5, 4, 3]
-    "preference-ordered list of nice step sizes"
+    Q = [1, 5, 2, D('2.5'), 4, 3]
+    """
+    preference-ordered list of nice step sizes
+
+    Optimally, values should be in an exact format, i.e. of type `int` or
+    `decimal.Decimal`.
+    """
 
     w = [0.25, 0.2, 0.5, 0.05]
     "weights for the subscores simplicity, coverage, density, and legibility"
@@ -29,20 +35,20 @@ class ExtendedWilkinson:
 
     # scoring functions, including the approximations for limiting the search
 
-    def simplicity(self, i, j, lmin, lmax, lstep):
+    def _simplicity(self, i, j, lmin, lmax, lstep):
         eps = float_info.epsilon * 100
         v = ((lmin % lstep < eps or lstep - (lmin % lstep) < eps)
              and lmin <= 0 and lmax >= 0) * 1
         return 1 - (i - 1) / (len(self.Q) - 1) - j + v
 
-    def simplicity_max(self, i, j):
+    def _simplicity_max(self, i, j):
         return 1 - (i - 1) / (len(self.Q) - 1) - j + 1
 
-    def coverage(self, dmin, dmax, lmin, lmax):
+    def _coverage(self, dmin, dmax, lmin, lmax):
         return (1 - 0.5 * ((dmax - lmax)**2 + (dmin - lmin)**2)
                 / (0.1 * (dmax - dmin))**2)
 
-    def coverage_max(self, dmin, dmax, span):
+    def _coverage_max(self, dmin, dmax, span):
         range = dmax - dmin
         if span > range:
             half = (span - range) / 2
@@ -50,39 +56,33 @@ class ExtendedWilkinson:
         else:
             return 1
 
-    def density(self, k, m, dmin, dmax, lmin, lmax):
+    def _density(self, k, m, dmin, dmax, lmin, lmax):
         r = (k - 1) / (lmax - lmin)
         rt = (m - 1) / (max(lmax, dmax) - min(dmin, lmin))
         return 2 - max((r / rt, rt / r))
 
-    def density_max(self, k, m):
+    def _density_max(self, k, m):
         return 2 - (k - 1) / (m - 1) if k >= m else 1
 
-    def legibility(self, lmin, lmax, lstep):
+    def _legibility(self, lmin, lmax, lstep):
         return 1
 
-    def score(self, s, c, d, l):
+    def _score(self, s, c, d, l):
         # combined score
         return self.w[0] * s + self.w[1] * c + self.w[2] * d + self.w[3] * l
 
     # optimization algorithm
 
-    def extended(self, dmin, dmax, m, only_loose):
+    def _extended(self, dmin, dmax, m, only_loose):
         eps = float_info.epsilon * 100
 
         if dmin > dmax:
             dmin, dmax = dmax, dmin
 
         if dmax - dmin < eps:
-            # If the range is near the floating point limit,
-            # generate some equally spaced steps.
-            best = dict(
-                lmin=dmin,
-                lmax=dmax,
-                lstep=(dmax - dmin) / (m - 1)
-            )
-            return best
+            return None
 
+        # threshold for optimization
         best = dict(score=-2)
 
         # We combine the j and q loops into one to enable breaking out of both
@@ -93,25 +93,25 @@ class ExtendedWilkinson:
                for j in count(start=1)
                for i, q in enumerate(self.Q, start=1))
         for j, i, q in JIQ:
-            sm = self.simplicity_max(i, j)
+            sm = self._simplicity_max(i, j)
 
-            if self.score(sm, 1, 1, 1) < best['score']:
+            if self._score(sm, 1, 1, 1) < best['score']:
                 break
 
             for k in count(start=2):      # loop over tick counts
-                dm = self.density_max(k, m)
+                dm = self._density_max(k, m)
 
-                if self.score(sm, 1, dm, 1) < best['score']:
+                if self._score(sm, 1, dm, 1) < best['score']:
                     break
 
-                delta = (dmax - dmin) / (k + 1) / (j * q)
+                delta = (dmax - dmin) / (k + 1) / (j * float(q))
 
                 for z in count(start=ceil(log10(delta))):
-                    step = q * j * 10**z
+                    step = float(q) * j * 10**z
 
-                    cm = self.coverage_max(dmin, dmax, step * (k - 1))
+                    cm = self._coverage_max(dmin, dmax, step * (k - 1))
 
-                    if self.score(sm, cm, dm, 1) < best['score']:
+                    if self._score(sm, cm, dm, 1) < best['score']:
                         break
 
                     min_start = floor(dmax / step) * j - (k - 1) * j
@@ -129,27 +129,42 @@ class ExtendedWilkinson:
                             if lmin > dmin or lmax < dmax:
                                 continue
 
-                        s = self.simplicity(i, j, lmin, lmax, lstep)
-                        c = self.coverage(dmin, dmax, lmin, lmax)
-                        d = self.density(k, m, dmin, dmax, lmin, lmax)
-                        l = self.legibility(lmin, lmax, lstep)                      # noqa E741
+                        s = self._simplicity(i, j, lmin, lmax, lstep)
+                        c = self._coverage(dmin, dmax, lmin, lmax)
+                        d = self._density(k, m, dmin, dmax, lmin, lmax)
+                        l = self._legibility(lmin, lmax, lstep)                      # noqa E741
 
-                        score = self.score(s, c, d, l)
+                        score = self._score(s, c, d, l)
 
                         if score > best['score']:
                             best = dict(
                                 lmin=lmin,
                                 lmax=lmax,
                                 lstep=lstep,
-                                score=score
+                                score=score,
+                                k=k,
+                                q=q,
+                                start=start,
+                                j=j,
+                                z=z
                             )
 
         return best
+        # without 'legibility' quite fast, 1.33 ms on average
 
-    def labels(self, dmin, dmax, m, only_loose=False):
-        best = self.extended(dmin, dmax, m, only_loose)
-        mp = round((best['lmax'] - best['lmin']) / best['lstep']) + 1
-        values = [index * best['lstep'] + best['lmin'] for index in range(mp)]
-        return best, values
+    def _values(self, q, start, j, z, k, **kwargs):
+        return [float(q) * (start + j * ind) * 10 ** z for ind in range(k)]
 
-# without 'legibility' quite fast, 1.33 ms on average
+    def _decimal_values(self, q, start, j, z, k, **kwargs):
+        return [q * (start + j * ind) * D(10) ** z for ind in range(k)]
+
+    def _labels(self, **kwargs):
+        decimal_values = self._decimal_values(**kwargs)
+        return ['{:f}'.format(dv.normalize()) for dv in decimal_values]
+
+    def ticks(self, dmin, dmax, m, only_loose=False):
+        best = self._extended(dmin, dmax, m, only_loose)
+        values = self._values(**best)
+        labels = self._labels(**best)
+        return values, labels, best
+
