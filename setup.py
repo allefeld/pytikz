f1d3958753fc062be3282905605a3bf78342b8b1
Carsten Allefeld <carsten.allefeld@city.ac.uk>
2024-02-26 06:38:28 +0000

    Explicitly specify encoding of README.md

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ python_requires = '>=3.6'
 install_requires = ['PyMuPDF','ipython','numpy']
 
 if __name__ == '__main__':
-    with open('README.md', 'r') as fh:
+    with open('README.md', 'r', encoding='utf-8') as fh:
         long_description = fh.read()
     long_description_content_type = 'text/markdown'
 
